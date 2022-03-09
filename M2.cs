4a74cd74f15c47db174874bcebe68a83744b30e2
MushfigCahatzade <99797351+MushfigCahatzade@users.noreply.github.com>
2022-03-09 14:30:54 -0800

    Add files via upload
    create task 2

---
+++ M2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Mushfig"));
+        }
+        static string Reverse(string word)
+        {
+            string result = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                result += word[i];
+            }
+            return result;
+        }
+    }
+}
