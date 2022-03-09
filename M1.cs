4187681107349fd3aa27d76aa994314dfe04bef1
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-03-09 14:07:37 -0800

    Create Task

---
+++ M1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=Eks(new int[] { -5, -40, 40, 35, 89, -98 });
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+                  
+            static int[] Eks(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    arr[i]=arr[i]*(-1);
+                }
+               
+            }
+            return arr;
+        }
+        
+    }
+}
+
+
